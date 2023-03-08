9120c753bf54c369425d295e053a643a640c55c2
advprogfund <gabrielfernandez@ufl.edu>
2023-03-08 14:46:14 -0500

    Decode function written, completing the program functionality.

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ def encode(password):
     return encoded_password
 
 
+def decode(password):
+    """Decoding a password by decrementing each value by 3"""
+    decoded_password = ""  # An empty string representing what will be the decoded_password; a new string object is necessary to return a decoded password without modifying password itself.
+
+    for num in password:
+        inner_num = int(num)  # int(...) ensures num, and by extension inner_num, is numeric.
+        if (inner_num - 3 < 0):  # As an "inverse" to % 10, if subtraction by the value (three in this case) yields a value below 0, add 10 so the resulting digit is within 0-9.
+            inner_num += 10
+        decoded_num = (inner_num - 3)  # The opposite operation of the encode (+3) is -3. This shift decodes the number...
+        decoded_password += str(decoded_num)  # ...which can then be concatenated to decoded_password.
+
+    return decoded_password
+
 def main():
 
     menu_selection = 1
@@ -33,7 +46,7 @@ def main():
 
         elif menu_selection == 2:  # Decode a password
             print(f"The encoded password is {encoded_password}, and "
-                  f"the original password is {decoded(encoded_password)}.")
+                  f"the original password is {decode(encoded_password)}.")
 
         elif menu_selection == 3:  # Quit the program
             break
